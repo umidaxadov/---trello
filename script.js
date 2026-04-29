36ce90cf938d42f29666a02fea1f93465ab398c7
umidaxadov <umidaxadov15@gmail.com>
2026-04-30 01:26:26 +0500

    js

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ function addTask() {
         value = ''
         form.style.display = 'none'
         btn.style.display = 'flex'
+
+        dargNdrop()
     })
 }
 
@@ -61,6 +63,7 @@ function addBoard() {
     boards.append(board)
 
     chageTitle()
+    dargNdrop()
 }
 button.addEventListener('click', addBoard)
 
@@ -71,4 +74,57 @@ function chageTitle() {
         title.addEventListener('click', e => e.target.textContent = '')
     })
 }
+
 chageTitle()
+
+let draggedItem = null
+
+
+function dargNdrop() {
+    const listItems = document.querySelectorAll('.list__item')
+    const lists = document.querySelectorAll('.list')
+
+    for (let i = 0; i < listItems.length; i++) {
+        const item = listItems[i]
+
+        item.addEventListener('dragstart', () => {
+            draggedItem = item
+            setTimeout(() => {
+                item.style.display = 'none'
+            }, 0)
+        })
+
+        item.addEventListener('dragend', () => {
+            setTimeout(() => {
+                item.style.display = 'block'
+                draggedItem = null
+            })
+        })
+
+        item.addEventListener('dblclick' , () => {
+            item.remove()
+        })
+
+        for (let j = 0; j < lists.length; j++) {
+            const list = lists[j]
+
+            list.addEventListener('dragover', e => {e.preventDefault()})
+
+
+            list.addEventListener('dragenter', function (e){
+                e.preventDefault()
+                this.style.backgroundColor = 'rgba(0,0,0,.3)'
+            })
+
+            list.addEventListener('dragleave', function(e) {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+            })
+
+            list.addEventListener('drop', function(e) {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+                this.append(draggedItem)
+            }) 
+        }
+    }
+}
+dargNdrop()
